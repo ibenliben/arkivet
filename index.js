8286d85ac6f177c66c601e12f63a7b4e19d06eb9
ibenliben <152152694+ibenliben@users.noreply.github.com>
2024-04-19 10:19:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const logo = document.getElementById("circle")
+const nav = document.getElementById("navbar")
+
+logo.addEventListener("click", () => {
+    console.log("logo ble trykket")
+
+    logo.classList.toggle("hide") /* FUNKER IKKE!!? */
+    nav.classList.toggle("show")
+}) 
+
+/* ARTIKKEL */
+
+const pil = document.querySelector(".fa-play")
+const artikkel = document.querySelector("article")
+
+pil.addEventListener("click", () => {
+    console.log(`"vis mer"-pil ble trykket`)
+
+    artikkel.classList.toggle("block")
+    pil.classList.toggle("fa-rotate-270")
+    pil.style.transition = "1s"
+})
