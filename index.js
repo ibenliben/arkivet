3df69dfc534793dd2fb95803c03c4f893f2002b2
ibenliben <iben@reiremo.no>
2024-04-20 14:08:44 +0200

    tidslinje

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const nav = document.getElementById("navbar")
 
 logo.addEventListener("click", () => {
     console.log("logo ble trykket")
-    
+
     nav.classList.toggle("show")
 }) 
 
@@ -19,3 +19,28 @@ pil.addEventListener("click", () => {
     pil.classList.toggle("fa-rotate-270")
     pil.style.transition = "1s"
 })
+
+/* TIDSLINJE */
+
+let items = document.querySelectorAll(".timeline li")
+
+function elementInView(el) {
+    let rect = el.getBoundingClientRect()
+
+    return (
+        rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    )
+}
+
+function callBack() {
+    for(let i = 0; i < items.length; i++) {
+        if(elementInView(items[i])) {
+            items[i].classList.add("in-view")
+        }
+    }
+}
+
+window.onload = callBack
+window.onresize = callBack
+window.onscroll = callBack
